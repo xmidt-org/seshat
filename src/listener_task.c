0b4abc5962b0ce31838f3545e7fbb46bff1bb9c6
Ramki Ananthakrishnan <ramki_ananthakrishnan@cable.comcast.com>
2017-03-06 13:37:41 -0800

    Added cJSON interface as a first draft for review. Unit tests to follow.

--- src/listener_task.c
+++ src/listener_task.c
@@ -1,34 +1,58 @@
-#include "listener_task.h"
-#include "wrp_interface.h"
+/**
+ * Copyright 2017 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 #include <pthread.h>
 
+#include "listener_task.h"
+#include "wrp_interface.h"
 
-
+/*----------------------------------------------------------------------------*/
+/*                            File Scoped Variables                           */
+/*----------------------------------------------------------------------------*/
 static pthread_t thread_id;
-
-static void *listener(void *data);
 static listener_data_t the_data;
+
+/*----------------------------------------------------------------------------*/
+/*                             Function Prototypes                            */
+/*----------------------------------------------------------------------------*/
+static void *listener(void *data);
+
+/*----------------------------------------------------------------------------*/
+/*                             External Functions                             */
+/*----------------------------------------------------------------------------*/
 int listener_thread_start(listener_data_t *data)
 {
     the_data = *data;
     return pthread_create(&thread_id, NULL, listener, (void *) &the_data);
 }
 
-
 void *listener(void *data)
 {
     listener_data_t in_data = * (listener_data_t *) data;
     char *buf = NULL;
     
-    while (1) {
+    while( 1 ) {
         int bytes = nn_recv (in_data.socket, &buf, NN_MSG, 0);
-        if (bytes > 0) {
+        if( bytes > 0 ) {
             wrp_msg_t response;
             response.msg_type = WRP_MSG_TYPE__AUTH;
-             printf("listener got %d bytes\n", bytes);
-             response.u.auth.status = create_response_to_message(buf, bytes);
-             nn_send(in_data.socket, &response, sizeof(wrp_msg_t), 0);
-             free(buf);
+            printf("listener got %d bytes\n", bytes);
+            response.u.auth.status = create_response_to_message(buf, bytes);
+            nn_send(in_data.socket, &response, sizeof(wrp_msg_t), 0);
+            free(buf);
         } else {
             // do we need to check for socket error other than timed out ?
             printf("listener timed out or socket error?\n");
--- src/wrp_interface.c
+++ src/wrp_interface.c
@@ -1,17 +1,106 @@
+/**
+ * Copyright 2017 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include <stdio.h>
+
+#include "cjson_interface.h"
 #include "wrp_interface.h"
 
-size_t create_response_to_message(char *data, size_t cnt)
+/*----------------------------------------------------------------------------*/
+/*                               Data Structures                              */
+/*----------------------------------------------------------------------------*/
+typedef struct wrp_svc_registration_msg reg_msg_t;
+typedef struct wrp_crud_msg             ret_msg_t;
+
+/*----------------------------------------------------------------------------*/
+/*                             Function Prototypes                            */
+/*----------------------------------------------------------------------------*/
+size_t file_to_string( FILE *handle, char **buffer );
+void process_message_reg( reg_msg_t *msg, char **buf );
+char *process_message_ret( ret_msg_t *msg, char *buf );
+
+/*----------------------------------------------------------------------------*/
+/*                             External Functions                             */
+/*----------------------------------------------------------------------------*/
+size_t create_response_to_message( char *data, size_t cnt, FILE *file_handle )
 {
     wrp_msg_t *msg;
     size_t response_code = 400;
     
-    if (0 < wrp_to_struct(data, cnt, WRP_BYTES, &msg)) {
-      if(msg->msg_type == WRP_MSG_TYPE__SVC_REGISTRATION || msg->msg_type == WRP_MSG_TYPE__RETREIVE) {
-    //   record_it();
-         response_code = 200;
-      }
-    wrp_free_struct(msg);
+    if( 0 < wrp_to_struct(data, cnt, WRP_BYTES, &msg) ) 
+    {
+        if( msg->msg_type == WRP_MSG_TYPE__SVC_REGISTRATION || msg->msg_type == WRP_MSG_TYPE__RETREIVE ) 
+        {
+            char *buf;
+            file_to_string(file_handle, &buf);
+            if( WRP_MSG_TYPE__SVC_REGISTRATION == msg->msg_type ) {
+                process_message_reg( &(msg->u.reg), &buf );
+            } else {
+                char *object = process_message_ret( &(msg->u.crud), buf );
+                (void) object;
+                /* TODO - Retrieved JSON object should be sent to requester. */
+            }
+            response_code = 200;
+            fwrite(buf, sizeof(char), strlen(buf), file_handle);
+            free(buf);
+        }
+        wrp_free_struct(msg);
     }
     
     return response_code;
+}
+
+/*----------------------------------------------------------------------------*/
+/*                             Internal functions                             */
+/*----------------------------------------------------------------------------*/
+size_t file_to_string( FILE *handle, char **buffer )
+{
+    size_t length;
+    if( NULL == handle )
+    {
+        return 0;
+    }
+
+    fseek(handle, 0, SEEK_END);
+    length = ftell(handle);
+    fseek(handle, 0, SEEK_SET);
+    *buffer = (char *) malloc(length);
+    if( NULL == buffer )
+    {
+        return 0;
+    }
+    return fread(*buffer, 1, length, handle);
+}
+
+void process_message_reg( reg_msg_t *msg, char **buffer )
+{
+    char *service = msg->service_name;
+    char *url     = msg->url;
+
+    cji_add_entry(service, url, buffer);
+}
+
+char *process_message_ret( ret_msg_t *msg, char *buffer )
+{
+    char *service = (char *)msg->payload;
+    char *object = NULL;
+
+    if( CJIRT__SUCCESS == cji_retrieve_entry(service, buffer, &object) )
+    {
+        return object;
+    }
+    return NULL;
 }
